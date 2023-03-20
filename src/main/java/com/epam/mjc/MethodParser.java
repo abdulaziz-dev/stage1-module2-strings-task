d05fcdba942c1c06ca99eb0f45fa5dbd664a7925
abdulaziz-dev <axmedovabdulaziz2001@gmail.com>
2023-03-20 19:20:23 +0500

    Method Signature Parsing

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,11 @@
 package com.epam.mjc;
 
+import com.epam.mjc.MethodSignature.Argument;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class MethodParser {
 
     /**
@@ -20,6 +26,48 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        //throw new UnsupportedOperationException("You should implement this method.");
+        String accessModifier = "";
+        String returnType;
+        String methodName;
+
+
+        String[] str = signatureString.split("\\(");
+        StringTokenizer st1 = new StringTokenizer(str[0], " ");
+
+        if (st1.countTokens() == 3){
+            accessModifier = st1.nextToken();
+            returnType = st1.nextToken();
+            methodName = st1.nextToken();
+        } else {
+            returnType = st1.nextToken();
+            methodName = st1.nextToken();
+        }
+
+        str[1] = str[1].substring(0,str[1].length()-1);
+        StringTokenizer st2 = new StringTokenizer(str[1], " ");
+
+        List<Argument> args = new ArrayList<>();
+        while (st2.hasMoreTokens()){
+            String type = st2.nextToken();
+            String name = st2.nextToken();
+            if( name.endsWith(",") ) {
+                name = name.substring(0,name.length()-1);
+            }
+            Argument arg = new Argument(type, name);
+            args.add(arg);
+        }
+        if (args.size() > 0){
+            MethodSignature ms = new MethodSignature(methodName, args);
+            ms.setReturnType(returnType);
+            ms.setAccessModifier(accessModifier.equals("") ? null : accessModifier);
+            return ms;
+        } else {
+            MethodSignature ms = new MethodSignature(methodName);
+            ms.setAccessModifier(accessModifier.equals("") ? null : accessModifier);
+            ms.setReturnType(returnType);
+            return ms;
+        }
+
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,7 +1,10 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringSplitter {
 
@@ -13,6 +16,10 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        //throw new UnsupportedOperationException("You should implement this method.");
+        String reg = String.join("|", delimiters);
+        return Arrays.stream(source.split(reg))
+                .filter(s -> !s.isBlank())
+                .collect(Collectors.toList());
     }
 }
